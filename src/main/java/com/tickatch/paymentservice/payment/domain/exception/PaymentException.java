ccac44e3e7424c05b6218e2c62ef3cb42ad92e61
sojsnake <sojsnake@naver.com>
2025-12-10 17:13:17 +0900

    feat: 에러 코드 추가#1

---
+++ src/main/java/com/tickatch/paymentservice/payment/domain/exception/PaymentException.java
@@ -0,0 +1,20 @@
+package com.tickatch.paymentservice.payment.domain.exception;
+
+import io.github.tickatch.common.error.BusinessException;
+import io.github.tickatch.common.error.ErrorCode;
+
+public class PaymentException extends BusinessException {
+
+  public PaymentException(ErrorCode errorCode) {
+    super(errorCode);
+  }
+
+  public PaymentException(ErrorCode errorCode, Object... errorArgs) {
+    super(errorCode, errorArgs);
+  }
+
+  public PaymentException(ErrorCode errorCode, Throwable cause,
+      Object... errorArgs) {
+    super(errorCode, cause, errorArgs);
+  }
+}
